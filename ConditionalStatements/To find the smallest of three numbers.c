10ede02f8fff80c4eee5591912842d678d1b7cb5
sanjay000627 <sanjay000627@gmail.com>
2026-02-28 15:58:37 +0530

    Create To find the smallest of three numbers.c

---
+++ ConditionalStatements/To find the smallest of three numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+
+    int num1, num2, num3;
+    scanf("%d %d %d", &num1, &num2, &num3);
+
+    if (num1 < num2 && num1 < num3){
+        printf("The 1st Number is the Smallest among three. \n");
+    }
+    else if (num2 < num1 && num2 < num3){
+        printf("The 2nd Number is the Smallest among three \n");
+    }
+    else{
+        printf("The 3rd Number is the Smallest among three \n");
+    }
+}
