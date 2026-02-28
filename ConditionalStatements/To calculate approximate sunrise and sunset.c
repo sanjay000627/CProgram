b04fbbbc658b82ab62c93e61d70ca5e7fb0ef7d5
sanjay000627 <sanjay000627@gmail.com>
2026-02-28 11:52:24 +0530

    Update To calculate approximate sunrise and sunset.c

--- ConditionalStatements/To calculate approximate sunrise and sunset.c
+++ ConditionalStatements/To calculate approximate sunrise and sunset.c
@@ -6,14 +6,17 @@ int main() {
         printf("Sunrise : 7:00 AM\n");
         printf("Sunset : 5:30 PM");
     }
+        
     else if(month==3){
          printf("Sunrise : 6:30 AM\n");
         printf("Sunset : 6:30 PM");
     }
+        
     else if(month==4 || month==5 || month==6 || month==7 || month==8){
          printf("Sunrise : 5:30 AM\n");
         printf("Sunset : 7:30 PM");
     }
+        
     else{
          printf("Sunrise : 6:30 AM\n");
         printf("Sunset : 6:00 PM");
